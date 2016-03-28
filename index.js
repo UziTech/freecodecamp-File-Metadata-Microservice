c5c6359651f39223a96b7e746230b556d47856c1
Tony Brix <Tony@Brix.ninja>
2016-03-27 22:08:41 -0500

    working

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-var express = require('express');
+var express = require("express");
+var upload = require("multer")();
 
 var PORT = (process.env.PORT || 5000);
 
 var app = express();
 
-app.use(function (req, res) {
-	// TODO: code here
+app.use(express.static("public"));
+
+app.post("/api/fileanalyse/", upload.any(), function (req, res) {
+	res.json(req.files[0]);
 });
 
 app.listen(PORT, function () {
-	console.log('Node app is running on port', PORT);
+	console.log("Node app is running on port", PORT);
 });
